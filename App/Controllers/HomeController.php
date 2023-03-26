e1df71c9d7cdece639eeefb26a545db27ed71b77
Katažina <katazina.fukalova@gmail.com>
2023-03-26 19:02:01 +0300

    Git init

---
+++ App/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controllers;
+use App\App;
+
+class HomeController {
+
+public function home()
+    {
+        return App::view('home/index', [
+            'title'=> 'Home'
+        ]);
+    }
+
+}
